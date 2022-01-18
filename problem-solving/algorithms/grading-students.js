eacc84f1ec14f3ad173ce87ca200df0fde3b1fe7
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-01-18 15:43:33 -0600

    Grading Students Solved

--- problem-solving/algorithms/grading-students.js
+++ problem-solving/algorithms/grading-students.js
@@ -8,7 +8,11 @@
  */
 
 const gradingStudents = (grades) => {
-
+    return grades.map(grade => {
+        const nearestMultiplyOfFive = Math.ceil(grade / 5) * 5
+        if (grade < 38) return grade
+        return (nearestMultiplyOfFive - grade) < 3 ? nearestMultiplyOfFive : grade 
+    })
 }
 
-console.log()
+console.log(gradingStudents([73, 67, 38, 33]))
