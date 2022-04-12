4752d8c876aa8349a6423323c8d40fb7e1a8897e
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-04-11 21:23:25 -0600

    between-two-sets problem

--- problem-solving/algorithms/between-two-sets.js
+++ problem-solving/algorithms/between-two-sets.js
@@ -5,9 +5,43 @@
  * These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
  */
 
-const betweenTwoSets = (arr1, arr2) => {
-    console.log(arr1)
-    console.log(arr2)
+const getGCD = (n1, n2) => {
+  if (n2 === 0) return n1;
+  return getGCD(n2, n1 % n2)
 }
 
-betweenTwoSets([2, 6], [5, 6])
+const getLCM = (n1, n2) => {
+  if (n1 === 0 || n2 === 0) return 0;
+  return (n1 * n2) / getGCD(n1, n2);
+}
+
+const betweenTwoSets = (arr1, arr2) => {
+  let result = 0;
+
+  let lcm = arr1[0];
+
+  arr1.forEach(element => {
+    lcm = getLCM(lcm, element);
+  });
+
+  let gcd = arr2[0];
+
+  arr2.forEach(element => {
+    gcd = getGCD(gcd, element);
+  })
+
+  let multiple = 0
+
+  while (multiple <= gcd) {
+    multiple += lcm;
+
+    if (gcd % multiple === 0) {
+      result++;
+    }
+  }
+
+  return result
+
+}
+
+console.log(betweenTwoSets([2, 4], [16, 32, 96]))
