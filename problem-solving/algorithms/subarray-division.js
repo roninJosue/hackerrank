8c0a103b9dfce159c039f5c8cd862d03bcc88673
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-04-19 21:36:45 -0600

    birthday problem

---
+++ problem-solving/algorithms/subarray-division.js
@@ -0,0 +1,16 @@
+/**
+ Two children, Lily and Ron, want to share a chocolate bar. Each of the squares has an integer on it.
+
+ Lily decides to share a contiguous segment of the bar selected such that:
+
+ * The length of the segment matches Ron's birth month, and,
+ * The sum of the integers on the squares is equal to his birthday.
+
+ Determine how many ways she can divide the chocolate.
+ */
+
+const birthday = (s, d, m) => {
+
+}
+
+console.log(birthday([2, 2, 1, 3, 2], 4, 3))
