7c0fbdff02c8c8e6f693ec5474bd2a8656851cd3
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-04-22 22:11:16 -0600

    Migratory birds problem

---
+++ problem-solving/algorithms/migratory-birds.js
@@ -0,0 +1,9 @@
+/**
+ * Given an array of bird sightings where every element represents a bird type id,
+ * determine the id of the most frequently sighted type.
+ * If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
+ */
+
+const migratoryBirds = (arr) => {
+
+};
