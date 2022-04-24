8f5a5cdfe06d21a16e47b1cd01c243f8e49d3164
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-04-23 22:03:27 -0600

    Migratory birds problem

--- problem-solving/algorithms/migratory-birds.js
+++ problem-solving/algorithms/migratory-birds.js
@@ -5,5 +5,15 @@
  */
 
 const migratoryBirds = (arr) => {
-
+  const birdCount = {};
+  arr.forEach((bird) => {
+    if (birdCount[bird]) {
+      birdCount[bird] += 1;
+    } else {
+      birdCount[bird] = 1;
+    }
+  });
+  const max = Math.max(...Object.values(birdCount));
+  const maxKeys = Object.keys(birdCount).filter((key) => birdCount[key] === max);
+  return Math.min(...maxKeys);
 };
