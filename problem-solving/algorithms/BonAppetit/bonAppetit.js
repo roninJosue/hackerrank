863c9bbbdb03e28e46b697ac6f4b76887ba65429
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-05-01 21:44:03 -0600

    Bon Appetit problem refactor

--- problem-solving/algorithms/BonAppetit/bonAppetit.js
+++ problem-solving/algorithms/BonAppetit/bonAppetit.js
@@ -1,8 +1,5 @@
 const bonAppetit = (bill, k, b) => {
-  const total = bill.reduce((acc,item, index) => {
-    if (index !== k) return acc + item
-    return acc
-  }, 0) / 2
+  const total = bill.reduce((acc, item, index) => index !== k ? acc + item : acc, 0) / 2
   console.log(`${total === b ? 'Bon Appetit' : b - total}`)
 }
 
