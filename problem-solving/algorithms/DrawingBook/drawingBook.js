a980d39d4ecb9aa36969fbed585f4dcb54f0afda
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-05-09 22:13:51 -0600

    Passing the test

--- problem-solving/algorithms/DrawingBook/drawingBook.js
+++ problem-solving/algorithms/DrawingBook/drawingBook.js
@@ -1,32 +1,26 @@
 const pageCount = (n, p) => {
-  const pages = Array.from({length: n}, (_, i) => i + 1)
-  const pagesReverse = [...pages].reverse()
   let count = 0;
-  let count2 = 0
+  let count2 = 0;
+  let page = 1;
 
-  for (let index = 0; index < n; index++) {
-    if (index === 0 && pages[index] === p) {
-      break;
-    } else if ((p === pages[index] || p === pages[index + 1]) && index > 0) {
-      break;
-    }
-    count++
+  while (page < p) {
+    page += 2;
+    count++;
   }
 
-  for (let index = 0; index < n; index++) {
-    if (index === 0 && pagesReverse[index] === p) {
-      break;
-    } else if ((p === pagesReverse[index] || p === pagesReverse[index + 1]) && index > 0) {
-      break;
-    }
-    count2++
+  if (n % 2 !== 0) {
+    page = n - 1
+  } else {
+    page = n
   }
 
-  console.log(pages)
-  console.log(pagesReverse)
-  console.log(count, count2)
-  return count > count2 ? count2 : count
+  while (page > p) {
+    count2++;
+    page -= 2;
+  }
+
+  return Math.min(count, count2);
 }
 
 //console.log(pageCount(6, 2))
-console.log(pageCount(5, 4))
+console.log(pageCount(6, 2))
