1cb1a62fe287ff31b1a20a34d13a4830a3d9b86f
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-06-13 22:45:47 -0600

    The hurdle Race problem refactor

--- problem-solving/algorithms/TheHurdleRace/hurdleRace.js
+++ problem-solving/algorithms/TheHurdleRace/hurdleRace.js
@@ -1,8 +1,4 @@
-const hurdleRace = (k, height) => {
-  const max = Math.max(...height)
-
-  return k > max ? 0 : max - k
-}
+const hurdleRace = (k, height) => Math.max(0, Math.max(...height) - k)
 
 console.log(hurdleRace(7, [2, 5, 4, 5, 2]))
 console.log(hurdleRace(4, [1, 6, 3, 5, 2]))
