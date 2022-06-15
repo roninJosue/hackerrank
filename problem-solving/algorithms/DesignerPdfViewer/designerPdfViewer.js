1fe4aa29f3b4270776f1004dba96671ba6ee9897
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-06-14 22:32:57 -0600

    Designer PDF viewer problem refactoring, using forEach instead of for loop

--- problem-solving/algorithms/DesignerPdfViewer/designerPdfViewer.js
+++ problem-solving/algorithms/DesignerPdfViewer/designerPdfViewer.js
@@ -1,12 +1,13 @@
 const designerPdfViewer = (h, word) => {
   const n = word.length
-  const heightArr = []
-  for (let index = 0; index < word.length; index++) {
-    const pos = word.charCodeAt(index) - 97
-    heightArr.push(h[pos])
-  }
+  let maxHeight = -1
 
-  return Math.max(...heightArr) * n
+  word.split('').forEach(w => {
+    const pos = w.charCodeAt(0) - 97
+    maxHeight = maxHeight < h[pos] ? h[pos] : maxHeight
+  })
+
+  return maxHeight * n
 }
 
 console.log(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], 'abc'))
