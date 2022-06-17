967d03380eaba8574bf75232bbdfe0d4b6ccca92
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-06-16 22:40:51 -0600

    Beautiful Days problem refactor, changing variable names

--- problem-solving/algorithms/BeautifulDaysAtTheMovies/beautifulDays.js
+++ problem-solving/algorithms/BeautifulDaysAtTheMovies/beautifulDays.js
@@ -1,7 +1,7 @@
 const beautifulDays = (i, j, k) => {
-  return Array.from({length: j - i + 1}, (_, y) => i + y).filter(n=>{
-    const reverse = Number(n.toString().split('').reverse().join(''))
-    return Math.abs(n - reverse) % k === 0
+  return Array.from({length: j - i + 1}, (_, y) => i + y).filter(number => {
+    const reversedNumber = Number(number.toString().split('').reverse().join(''))
+    return Math.abs(number - reversedNumber) % k === 0
   }).length
 }
 
