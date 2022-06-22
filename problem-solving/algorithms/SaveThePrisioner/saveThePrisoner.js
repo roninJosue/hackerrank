6b30e15985a0224723dbbdfb6f80cd46eba19242
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-06-22 11:37:02 -0600

    Save the prisoner problem resolved with optimization.

--- problem-solving/algorithms/SaveThePrisioner/saveThePrisoner.js
+++ problem-solving/algorithms/SaveThePrisioner/saveThePrisoner.js
@@ -1,19 +1,7 @@
 const saveThePrisoner = (n, m, s) => {
-  let chair = s
-
-  while (m > 1) {
-    if (chair === n) {
-      chair = 1
-    } else {
-      chair++
-    }
-    m--
-  }
-
-  return chair
+  const r = m % n;
+  const res = (r + s - 1) % n
+  return res === 0 ? n : res
 }
 
-console.log(saveThePrisoner(5, 2, 1))
-console.log(saveThePrisoner(5, 2, 2))
-console.log(saveThePrisoner(7, 19, 2))
-console.log(saveThePrisoner(3, 7, 3))
+console.log(saveThePrisoner(208526924, 756265725, 150817879))
