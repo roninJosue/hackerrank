44feb2db99ac6df7da122cc2e3ef2c4369efb29d
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-06-30 22:07:27 -0600

    Circular Array Rotation problem (optimized).

--- problem-solving/algorithms/CircularArrayRotation/circularArrayRotation.js
+++ problem-solving/algorithms/CircularArrayRotation/circularArrayRotation.js
@@ -1,17 +1,14 @@
 const circularArrayRotation = (a, k, queries) => {
   let newArr = []
-  for (let x = 1; x <= k; x++) {
-    newArr = []
+
     for (let index = 0; index < a.length; index++) {
-      const newIndex = (index + a.length + 1) % a.length
+      let newIndex = (k + index + 1) % a.length
+      if (newIndex === 0) newIndex = a.length - 1
+      else newIndex--
       newArr[newIndex] = a[index]
     }
-    a = [...newArr]
-
-  }
 
   return queries.map(q => newArr[q])
 }
 
 console.log(circularArrayRotation([1, 2, 3], 2, [0, 1, 2]))
-///console.log(circularArrayRotation([3, 1, 2], 2, [0, 1, 2]))
