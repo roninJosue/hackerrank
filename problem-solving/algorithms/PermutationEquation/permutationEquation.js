d1cd40473f83dd00c8cda5596cc87afe568747e8
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-07-25 21:35:35 -0600

    refactor: Algorithm implementation

--- problem-solving/algorithms/PermutationEquation/permutationEquation.js
+++ problem-solving/algorithms/PermutationEquation/permutationEquation.js
@@ -1,3 +1,15 @@
 const permutationEquation = p => {
-  return p.length
+  const n = p.length
+  const permutationArray = []
+
+  for (let index = 1; index <= n; index++) {
+    let i = p.findIndex(x => index === x)
+    let y = p.findIndex(z => (i + 1) === z)
+    permutationArray.push(y + 1)
+  }
+
+  return permutationArray
 }
+
+console.log(permutationEquation([5, 2, 1, 3, 4]))
+console.log(permutationEquation([2, 3, 1]))
