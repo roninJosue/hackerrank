3868dc16bdc2c95c8be9bba25b46632436887af0
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-08-02 14:39:18 -0600

    refactor(MaxSubsetSum): Sum all the non-adjacent elements.

--- interview-preparation-kit/dinamyc-programming/MaxArraySum/maxSubsetSum.js
+++ interview-preparation-kit/dinamyc-programming/MaxArraySum/maxSubsetSum.js
@@ -1,16 +1,16 @@
 const maxSubsetSum = (arr) => {
   let sum = arr[0];
   let previousSum = 0;
-  let tempSum;
 
   for (let index = 1; index < arr.length; index++) {
-      tempSum = sum
-      sum = Math.max(sum, previousSum + arr[index])
+      let tempSum = Math.max(sum, previousSum)
+      sum = previousSum + arr[index];
       previousSum = tempSum
   }
 
-  return sum
+  return Math.max(sum, previousSum)
 }
 
 console.log(maxSubsetSum([-2, 1, 3, -4, 5]))
 console.log(maxSubsetSum([3, 5, -7, 8, 10]))
+console.log(maxSubsetSum([-5]))
