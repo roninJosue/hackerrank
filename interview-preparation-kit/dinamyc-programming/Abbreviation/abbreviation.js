43fd82e7b9991a56840ee6d46dcce88e53e4d065
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-08-24 21:47:38 -0600

    refactor(Abbreviation): Initialize the first row of the 2D array

--- interview-preparation-kit/dinamyc-programming/Abbreviation/abbreviation.js
+++ interview-preparation-kit/dinamyc-programming/Abbreviation/abbreviation.js
@@ -2,38 +2,59 @@ const abbreviation = (a, b) => {
   const lenStringA = a.length
   const lenStringB = b.length
 
-  let DP = Array(lenStringB + 1)
+  let resolve = Array(lenStringA + 1)
     .fill(false)
-    .map(() => Array(lenStringA + 1).fill(false))
+    .map((e, i) => {
+      //console.log(`index: ${i}, value: ${e}`)
+      return Array(lenStringB + 1).fill(false)
+    })
 
-  DP[0][0] = true
+  resolve[0][0] = true
+  for (let i = 1; i <= lenStringA; i++) {
+    let isLower = a[i - 1].toLowerCase() === a[i - 1]
+    if (isLower) {
+      resolve[i][0] = resolve[i - 1][0]
+    } /*else {
+      DP[i][0] = false
+    }*/
+    //console.log(DP[i][0])
+  }
 
   for (let i = 1; i <= lenStringA; i++) {
     //console.log(a[i])
     for (let j = 1; j <= lenStringB; j++) {
       const isUpper = a[i - 1] === a[i - 1].toUpperCase()
-      console.log(`a=${a[i - 1]}, b=${b[j - 1]}`)
+      //console.log(`a[${i - 1}]=${a[i - 1]}, b[${j - 1}]=${b[j - 1]}`)
       if (a[i - 1].toUpperCase() === b[j - 1] && isUpper) {
-        DP[i][j] = DP[i - 1][j - 1]
+        resolve[i][j] = resolve[i - 1][j - 1]
         console.log('cond a')
+        console.log(`[${i}][${j}] = ${resolve[i - 1][j - 1]}`)
       } else if (a[i - 1].toUpperCase() === b[j - 1] && !isUpper) {
-        DP[i][j] = DP[i - 1][j - 1] || DP[i][j - 1]
+        resolve[i][j] = resolve[i - 1][j - 1] || resolve[i][j - 1]
         console.log('cond b')
+        console.log(`[${i}][${j}] = ${resolve[i - 1][j - 1] || resolve[i][j - 1]}`)
       } else if (a[i - 1].toUpperCase() !== b[j - 1] && isUpper) {
-        DP[i][j] = false
+        resolve[i][j] = false
         console.log('cond d')
-      } else {
-        DP[i][j] = DP[i][j - 1]
+        console.log(`[${i}][${j}] = false`)
+      } else if (a[i - 1].toUpperCase() !== b[j - 1] && !isUpper) {
+        resolve[i][j] = resolve[i][j - 1]
         console.log('cond c')
+        console.log(`[${i}][${j}] = ${resolve[i][j - 1]}`)
       }
     }
   }
 
-  console.log(DP)
+  console.log(resolve)
+  console.log(resolve[lenStringA][lenStringB])
 }
 
 console.time('abb')
 
+abbreviation('beFgH', 'EFH')
+//abbreviation('abbcB', 'B')
+//abbreviation('abcBc', 'B')
+//abbreviation('abAxxaBa', 'ABA')
 /*const test0 = abbreviation(
   'AfPZN',
   'APZNC'
@@ -97,11 +118,11 @@ const test8 = abbreviation(
 )
 console.log(test)*/
 
-const test = abbreviation(
+/*const test = abbreviation(
   'abAxxaBa',
   'ABA'
 )
-console.log(test)
+console.log(test)*/
 
 
 console.timeEnd('abb')
