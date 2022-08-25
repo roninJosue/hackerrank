6ec8f3c27da39d47dfad42f4ff593028298d27bf
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-08-24 23:21:31 -0600

    refactor(Abbreviation): Complete the tests

--- interview-preparation-kit/dinamyc-programming/Abbreviation/abbreviation.js
+++ interview-preparation-kit/dinamyc-programming/Abbreviation/abbreviation.js
@@ -1,129 +1,35 @@
 const abbreviation = (a, b) => {
-  const lenStringA = a.length
-  const lenStringB = b.length
 
-  let resolve = Array(lenStringA + 1)
+  let resolve = Array(b.length + 1)
     .fill(false)
-    .map((e, i) => {
-      //console.log(`index: ${i}, value: ${e}`)
-      return Array(lenStringB + 1).fill(false)
+    .map(() => {
+      return Array(a.length + 1).fill(false)
     })
 
   resolve[0][0] = true
-  for (let i = 1; i <= lenStringA; i++) {
+
+  for (let i = 1; i <= a.length; i++) {
     let isLower = a[i - 1].toLowerCase() === a[i - 1]
     if (isLower) {
-      resolve[i][0] = resolve[i - 1][0]
-    } /*else {
-      DP[i][0] = false
-    }*/
-    //console.log(DP[i][0])
+      resolve[0][i] = true
+    }
   }
 
-  for (let i = 1; i <= lenStringA; i++) {
-    //console.log(a[i])
-    for (let j = 1; j <= lenStringB; j++) {
-      const isUpper = a[i - 1] === a[i - 1].toUpperCase()
-      //console.log(`a[${i - 1}]=${a[i - 1]}, b[${j - 1}]=${b[j - 1]}`)
+
+  for (let i = 1; i <= a.length; i++) {
+    const isUpper = a[i - 1] === a[i - 1].toUpperCase()
+    for (let j = 1; j <= b.length; j++) {
       if (a[i - 1].toUpperCase() === b[j - 1] && isUpper) {
-        resolve[i][j] = resolve[i - 1][j - 1]
-        console.log('cond a')
-        console.log(`[${i}][${j}] = ${resolve[i - 1][j - 1]}`)
+        resolve[j][i] = resolve[j - 1][i - 1]
       } else if (a[i - 1].toUpperCase() === b[j - 1] && !isUpper) {
-        resolve[i][j] = resolve[i - 1][j - 1] || resolve[i][j - 1]
-        console.log('cond b')
-        console.log(`[${i}][${j}] = ${resolve[i - 1][j - 1] || resolve[i][j - 1]}`)
-      } else if (a[i - 1].toUpperCase() !== b[j - 1] && isUpper) {
-        resolve[i][j] = false
-        console.log('cond d')
-        console.log(`[${i}][${j}] = false`)
-      } else if (a[i - 1].toUpperCase() !== b[j - 1] && !isUpper) {
-        resolve[i][j] = resolve[i][j - 1]
-        console.log('cond c')
-        console.log(`[${i}][${j}] = ${resolve[i][j - 1]}`)
+        resolve[j][i] = resolve[j - 1][i - 1] || resolve[j][i - 1]
+      }  else if (a[i - 1].toUpperCase() !== b[j - 1] && !isUpper) {
+        resolve[j][i] = resolve[j][i - 1]
       }
     }
   }
 
-  console.log(resolve)
-  console.log(resolve[lenStringA][lenStringB])
+  return resolve[b.length - 1][a.length - 1] ? 'YES' : 'NO'
 }
 
-console.time('abb')
-
-abbreviation('beFgH', 'EFH')
-//abbreviation('abbcB', 'B')
-//abbreviation('abcBc', 'B')
-//abbreviation('abAxxaBa', 'ABA')
-/*const test0 = abbreviation(
-  'AfPZN',
-  'APZNC'
-)
-
-console.log(`test0: ${test0}`)*/
-
-/*const test1 = abbreviation(
-  'SVAHHHMVIIDYIcOSHMDUAVJRIBxBZQSUBIVEBHfVTZVSHATUYDJGDRRUBQFHEEEUZLQGXTNKFWUYBAeFKUHSFLZEUINBZYRIXOPYYXAEZZWELUPIEIWGZHEIYIROLQLAVHhMKRDSOQTJYYLTCTSIXIDAnPIHNXENWFFZFJASRZRDAPVYPAViVBLVGRHObnwlcyprcfhdpfjkyvgyzpovsgvlqbhtwrucvszaqinbgeafuswkjrcexvyzq',
-  'SVAHHHMVIIDYIOSHMDUAVJRIBBZQSUBIVEBHVTZVSHATUYDJGDRRUBQFHEEEUZLQGXTNKFWUYBAFKUHSFLZEUINBZYRIXOPYYXAEZZWELUPIEIWGZHEIYIROLQLAVHMKRDSOQTJYYLTCTSIXIDAPIHNXENWFFZFJASRZRDAPVYPAVVBLVGRHO'
-)
-
-const test2 = abbreviation(
-  'WIRUQDCAQUTtZVVAzLKMVWIOXLBESKUQSUOAEHEFMMhwwiyrqhaiixtqhaynwcsoihabohxepjbyroaglaobfwwzymlijsdhdsqlxazqggesnhdnblqbgouwjyowpjfmbshfenrtcoqyszmpamoaehnepi',
-  'WIRUQDCAQUTZVVALKMVWIOXLBESKUQSUOAEHEFMM'
-)
-
-const test3 = abbreviation(
-  'EPgDKTFPMjMXHIOYEFAFDWqNXNXCYOQwXOYDHNEJGBIKZYxiPrUALJFEVMRJEVKWZXNEMMPWAXMCAOjAsAGBhYTTKZHITGSYQQGyUMSLVDSFWOLJICNEWUBOMNOBOuFKUMREWGTObXRFKORKVUWNZMWCPXJGQNJETWHyKPNBYXLfWYUXQIXVVHFMGPNLcOKGRQFCADNCRmfBSbZJwLSWEpXvELKSDUMKsZlUHNYFEf',
-  'EPDKTFPMMXHIOYEFAFDWNXNXCYOQXOYDHNEJGBIKZYPUALJFEVMRJEVKWZXNEMMPWAXMCAOAAGBYTTKZHITGSYQQGUMSLVDSFWOLJICNEWUBOMNOBOFKUMREWGTOXRFKORKVUWNZMWCPXJGQNJETWHKPNBYXLWYUXQIXVVHFMGPNLOKGRQFCADNCRBSZJLSWEXELKSDUMKZUHNYFE'
-)
-
-const test4 = abbreviation(
-  'XWUTAMOTDYULFGPIZVLGEVUWTNCDFWMDMGOADGOGYWIKACOARBPBGCCEQZWCURWCDSFCCWVIRZJNVYWUXHNXABVQXUNGEQWSDJAMIQBDLRPHUVLMRXLDJIWKRWBDKIACCPGVXINRCSQNBWDVEVNVLGLSCCSALXRKICOGTOHYVXUUSMZCFMXUDEESPXPMBXRJSQGWBUXMXNJDWZVLWPVTNSJYVTPCDTWSCJFINKELQCDVPPXZISqmgnrzwt',
-  'XWUTAMOTDYULFGPIZVLGEVUWTNCDFWMDMGOADGOGYWIKACOARBPBGCCEQZWCURWCDSFCCWVIRZJNVYWUXHNXABVQXUNGEQWSDJAMIQBDLRPHUVLMRXLDJIWKRWBDKIACCPGVXINRCSQNBWDVEVNVLGLSCCSALXRKICOGTOHYVXUUSMZCFMXUDEESPXPMBXRJSQGWBUXMXNJDWZVLWPVTNSJYVTPCDTWSCJFINKELQCDVPPXZIS'
-)
-
-const test5 = abbreviation(
-  'ALRIGJCXcFvZDTYFKWEEJSpayhsytgccieascmcgdejyunrwguxnuxneurbgfegnfpwbxsuolsxchasjegjdhwpzmwsyocdfnoiajkxugbliytqsthglmkawozahjsrhwdojwnpyycbbgxwxlsbslapslyphdxqjdgpplhcpug',
-  'ALRIGJCXFZDTYFKWEEJS'
-)
-
-const test6 = abbreviation(
-  'QGBOZKPCPMvEHGOFRWOXUOBCZDADERSPXNNJPOjMNUIHFZRACZJZWMZSNHJHJUYNCHSJKFDSLFBGKVYFJdkfxesjrixmzefcgewgrnn',
-  'QGBOZKPCPMEHGOFRWOXUOBCZDADERSPXNNJPOMNUIHFZRACZJZWMZSNHJHJUYNCHSJKFDSLFBGKVYFJ'
-)
-
-const test7 = abbreviation(
-  'lHJCJMWDFhSMRRZUKEUJxRZGVWATJtFRJPOOWkFEIEKsYBVILWARcDISwXAVPOYJHWRNHUQNDBUSRGIRIZTMHdXxodmgzkfxbpperyhufxymrphzkyzewldrijewezvobdshebnbtzxkupcvgrscyscrtxuwsmywoyuuxvyzxajccydqdngartsmbeptpabthasbgqccvzqvbmnokjwwqpzqcqqb',
-  'HJCJMWDFSMRRZUKEUJRZGVWATJFRJPOOWFEIEKYBVILWARDISXAVPOYJHWRNHUQNDBUSRGIRIZTMHX'
-)
-
-const test8 = abbreviation(
-  'TIJTQKlOTDVMEMKKMNYZtZIWLZOLJPPGCGTGTQBKILAXYMDXOOQMONZYXDZSgSVGJBmIIXXaTOLdMLbSCODRIUANIBRIQLVSXokicxpigmnirkneh',
-  'TIJTQKOTDVMEMKKMNYZZIWLZOLJPPGCGTGTQBKILAXYMDXOOQMONZYXDZSSVGJBIIXXTOLMLSCODRIUANIBRIQLVSX'
-)*/
-
-// const test9 = abbreviation(
-//   'MCLGDNAYZOVXJqJDAYrFHrBYZOUDBGEFfBTBZpNEZLURWEcDEBJQWIITUTNEteBHYYKOEGROMXmLEudSEWlUEFFTvINEHWNFObNOMWTNJMIyBEXTWBWOAKHNZKeEdCFuUSfAPXXZUZTDCVYmDFTSSGECgOHOQHSPEBUUWTspaviedpoeh',
-//   'MCLGDNAYZOVXJJDAYFHBYZOUDBGEFBTBZNEZLURWEDEBJQWIITUTNEBHYYKOEGROMXLESEWUEFFTINEHWNFONOMWTNJMIBEXTWBWOAKHNZKECFUSAPXXZUZTDCVYDFTSSGECOHOQHSPEBUUWT'
-// )
-
-// const test = abbreviation(
-//   'NZKeEdCF',
-//   'NZKECF'
-// )
-
-/*const test = abbreviation(
-  'abAxxaBa',
-  'ABA'
-)
-console.log(test)*/
-
-/*const test = abbreviation(
-  'abAxxaBa',
-  'ABA'
-)
-console.log(test)*/
-
-
-console.timeEnd('abb')
-
+console.log(abbreviation('daBcd', 'ABC'))
