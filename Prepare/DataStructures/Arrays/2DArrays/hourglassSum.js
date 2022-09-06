aa372dfa294633f3be55ed2180bfbefc1d2a9020
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-09-05 20:12:36 -0600

    feat(2DArray): Add solution to the problem

--- Prepare/DataStructures/Arrays/2DArrays/hourglassSum.js
+++ Prepare/DataStructures/Arrays/2DArrays/hourglassSum.js
@@ -1,3 +1,20 @@
 const hourglassSum = arr => {
-  return 0
+  let sum = -63
+  for (let i = 0; i <= 3; i++) {
+    for (let j = 0; j <= 3; j++) {
+      let tempSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+      sum = tempSum > sum ? tempSum : sum
+    }
+  }
+
+  return sum
 }
+
+hourglassSum([
+  [1, 1, 1, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0],
+  [0, 0, 2, 4, 4, 0],
+  [0, 0, 0, 2, 0, 0],
+  [0, 0, 1, 2, 4, 0],
+])
