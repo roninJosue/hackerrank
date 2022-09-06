1d4158b5f5519270d8118d1bbab1264dc48ac8eb
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-09-05 20:16:16 -0600

    refactor(2DArray): Create variables to hold the difference sum

--- Prepare/DataStructures/Arrays/2DArrays/hourglassSum.js
+++ Prepare/DataStructures/Arrays/2DArrays/hourglassSum.js
@@ -2,7 +2,12 @@ const hourglassSum = arr => {
   let sum = -63
   for (let i = 0; i <= 3; i++) {
     for (let j = 0; j <= 3; j++) {
-      let tempSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+      const topSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+      const midSum = arr[i + 1][j + 1]
+      const bottomSum = arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
+
+      let tempSum = topSum + midSum + bottomSum
+
       sum = tempSum > sum ? tempSum : sum
     }
   }
