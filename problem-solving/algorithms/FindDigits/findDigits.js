540dd2f53ed4ab6a613d45cfdaf42f7fbf033ad6
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-09-07 08:20:17 -0600

    refactor(FindDigits): Use only one variable for split the number

--- problem-solving/algorithms/FindDigits/findDigits.js
+++ problem-solving/algorithms/FindDigits/findDigits.js
@@ -1,9 +1,9 @@
 const findDigits = n => {
-  const nString = n.toString()
-  const divideNumber = nString.split('')
+  const splitNumber = n.toString()
+    .split('')
   let count = 0
 
-  for (const digit of divideNumber) {
+  for (const digit of splitNumber) {
     count = n % parseInt(digit) === 0 ? count + 1 : count
   }
 
