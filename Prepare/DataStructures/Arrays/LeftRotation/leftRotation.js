1dbbacafc209b46143e9f3bf03e212392ce6d700
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-09-12 21:09:52 -0600

    refactor(LeftRotation): Implement algorithm

--- Prepare/DataStructures/Arrays/LeftRotation/leftRotation.js
+++ Prepare/DataStructures/Arrays/LeftRotation/leftRotation.js
@@ -1,3 +1,13 @@
 const leftRotation = (arr, d) => {
-  return []
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    let newIndex = (i - d) % arr.length
+    newArr[newIndex] = arr[i]
+  }
+
+  return Object.values(newArr).map(n=>{
+    return n
+  })
 }
+
+console.log(leftRotation([1, 2, 3, 4, 5], 4))
