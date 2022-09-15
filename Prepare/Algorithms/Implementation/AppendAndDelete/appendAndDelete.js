89774a49ee8c55d2b2c79097a17c91cd0201c73e
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-09-14 20:55:28 -0600

    feat(AppendAndDelete): Start implementing the algorithm

--- Prepare/Algorithms/Implementation/AppendAndDelete/appendAndDelete.js
+++ Prepare/Algorithms/Implementation/AppendAndDelete/appendAndDelete.js
@@ -1,3 +1,19 @@
 const appendAndDelete = (s, t, k) => {
-  return []
+  const sSize = s.length
+  const tSize = t.length
+  let counter = 0
+
+  //if (sSize > k) return 'NO'
+
+  for (let i = 0; i < sSize; i++) {
+    if (s[i] !== t[i]){
+      counter++
+    }
+    //console.log(`s[i] = ${s[i]}`)
+    //console.log(`t[i] = ${t[i]}`)
+  }
+
+  return counter === k ? 'YES' : 'NO'
 }
+
+console.log(appendAndDelete('hackerhappy', 'hackerrank', 9))
