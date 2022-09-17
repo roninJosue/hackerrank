4722d969e5b2485033be1cfeead5e1f63c1d5adc
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-09-16 22:24:22 -0600

    refactor(MigratoryBirds): Remove comments

--- problem-solving/algorithms/MigratoryBirds/migratory-birds.js
+++ problem-solving/algorithms/MigratoryBirds/migratory-birds.js
@@ -1,9 +1,3 @@
-/**
- * Given an array of bird sightings where every element represents a bird type id,
- * determine the id of the most frequently sighted type.
- * If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
- */
-
 const migratoryBirds = (arr) => {
   const birdCount = {};
   arr.forEach((bird) => {
