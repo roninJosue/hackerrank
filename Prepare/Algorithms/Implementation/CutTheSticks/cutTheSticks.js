dc818d6bf04645ecb00a92320c0590b0b82f501e
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-09-19 09:44:40 -0600

    refactor(CutTheSticks): Remove extra variable

--- Prepare/Algorithms/Implementation/CutTheSticks/cutTheSticks.js
+++ Prepare/Algorithms/Implementation/CutTheSticks/cutTheSticks.js
@@ -1,15 +1,13 @@
 const cutTheSticks = arr => {
   let minLengthStick = Math.min(...arr)
   let sticksCut = []
-  let size = arr.length
 
-  while (size > 0) {
+  while (arr.length > 0) {
     sticksCut.push(arr.length)
     arr = [...arr].map(stick => {
       return stick - minLengthStick
     }).filter(stick => stick > 0)
     minLengthStick = Math.min(...arr)
-    size = arr.length
   }
 
   return sticksCut
